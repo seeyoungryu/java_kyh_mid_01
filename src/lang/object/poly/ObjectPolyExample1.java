41036f1ea1926bdc3784c5f81d9443dbdc9f1b5e
seeyoungryu <seeyoungryu@gmail.com>
2024-06-23 01:33:59 +0900

    update pack.poly - Type Casting

--- src/lang/object/poly/ObjectPolyExample1.java
+++ src/lang/object/poly/ObjectPolyExample1.java
@@ -20,5 +20,13 @@ public class ObjectPolyExample1 {
         } else if (obj instanceof Car car) {
             car.move();
         }
+
+        /*
+        타입 캐스팅 (Type Casting):
+
+        obj는 Object 타입이기 때문에, 컴파일러는 obj가 sound()나 move() 메서드를 가지고 있다고 인식하지 않는다.
+        instanceof 연산자를 사용하여 obj의 실제 타입을 확인하고, 해당 타입으로 안전하게 캐스팅한다.
+         */
+
     }
 }
