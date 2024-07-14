350ca0d6276e4bcafc47746a6b65cad360bbd7d3
seeyoungryu <seeyoungryu@gmail.com>
2024-07-14 15:58:58 +0900

    update pack.immutable

--- src/lang/immutable/test/ImmutableMyDateMain.java
+++ src/lang/immutable/test/ImmutableMyDateMain.java
@@ -9,6 +9,17 @@ public class ImmutableMyDateMain {
         System.out.println("date1 = " + date1);
         System.out.println("date2 = " + date1);
 
+/*
+    참고 : withXxx()
+    불변 객체에서 값을 변경하는 경우 관례처럼 사용함
+     - 원본 객체의 상태가 그대로 유지됨을 강조하면서 변경사항을 새 복사본에 포함하는 과정을 의미
+
+        public ImmutableMyDate withYear(int newYear) {        //newYear로 받기 (변경 위함)
+            return new ImmutableMyDate(newYear, month, day);
+        }
+
+ */
+
         System.out.println("2025 - > date1");
         //date1.setYear(2025);
         date1 = date1.withYear(2025); ////date1=x002, date2=x001         //newYear
