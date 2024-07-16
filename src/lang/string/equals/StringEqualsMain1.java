eaf1c3a5ca21ea244f231071ac962a67061200dd
seeyoungryu <seayeux@gmail.com>
2024-07-16 15:01:00 +0900

    update pack.equals

--- src/lang/string/equals/StringEqualsMain1.java
+++ src/lang/string/equals/StringEqualsMain1.java
@@ -5,8 +5,8 @@ public class StringEqualsMain1 {
     public static void main(String[] args) {
         String str1 = new String("hello"); //x001
         String str2 = new String("hello"); //x002
-        System.out.println("new String() == 비교: " + (str1 == str2));
-        System.out.println("new String() equals 비교:" + (str1.equals(str2)));
+        System.out.println("new String() == 비교: " + (str1 == str2)); //인스턴스 비교 ㄴ
+        System.out.println("new String() equals 비교:" + (str1.equals(str2)));  //value 값 비교
 
         String str3 = "hello"; //x003
         String str4 = "hello"; //x004
