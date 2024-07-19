9255e474aefd1a729eed54c1704b4298ea02449f
seeyoungryu <seayeux@gmail.com>
2024-07-20 05:32:38 +0900

    update pack.time

--- src/nested/nested/NestedOuter.java
+++ src/nested/nested/NestedOuter.java
@@ -2,8 +2,16 @@ package nested.nested;
 
 public class NestedOuter {
 
-    private static int outClassValue = 3;
-    private int outInstanceValue = 2;
+    private static int outClassValue = 3; //바깥 클래스의 클래스 멤버
+    private int outInstanceValue = 2;  //바깥 클래스의 인스턴스 멤버
+
+    /* outInstanceValue 가 인스턴스 멤버인 이유
+       1. static 키워드가 없음
+       2. 인스턴스를 생성하지 않으면 이 변수에 접금할 수 없음(클래스의 각 인스턴스마다 개별적으로 존재)
+       - outInstanceValue는 static 키워드가 없기 때문에 클래스 멤버가 아닌 인스턴스 멤버입니다.
+       - 이 변수는 NestedOuter 클래스의 인스턴스가 생성될 때마다 각각의 인스턴스에 대해 별도로 존재합니다.
+     */
+
 
     static class Nested {
         private int nestedInstanceValue = 1;
